21b7647df42581229488eb8de02b2629e4837796
Addin Munawwar <moonawar@protonmail.com>
2025-06-20 16:09:57 +0700

    feat: subtitle

--- Assets/Scripts/Scene/SceneLifecycleHandler.cs
+++ Assets/Scripts/Scene/SceneLifecycleHandler.cs
@@ -1,11 +1,21 @@
+using System;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
+
+[Serializable]
+public class DelayedEvent
+{
+    public float Delay;
+    public UnityEvent Events;
+}
 
 public class SceneLifecycleHandler : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onAwake;
     [SerializeField] private UnityEvent _onStart;
     [SerializeField] private UnityEvent _onDestroy;
+    [SerializeField] private DelayedEvent[] _delayedEvents;
 
     private void Awake()
     {
@@ -15,6 +25,22 @@ public class SceneLifecycleHandler : MonoBehaviour
     private void Start()
     {
         _onStart?.Invoke();
+        if (_delayedEvents != null)
+        {
+            foreach (var delayedEvent in _delayedEvents)
+            {
+                if (delayedEvent != null && delayedEvent.Events != null)
+                {
+                    RunDelayedEvent(delayedEvent).Forget();
+                }
+            }
+        }
+    }
+
+    private async UniTaskVoid RunDelayedEvent(DelayedEvent delayedEvent)
+    {
+        await UniTask.Delay((int)(delayedEvent.Delay * 1000f));
+        delayedEvent.Events.Invoke();
     }
 
     private void OnDestroy()
---
+++ Assets/Scripts/UI/SubtitleUI.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+public class SubtitleUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI subtitleText;
+    [SerializeField] private float _dissapearAfter = 5f;
+    [SerializeField] private float _perCharacterTypeDuration = 0.05f;
+    [SerializeField] private float _pauseOnCommaDuration = 0.5f;
+
+    private CancellationTokenSource _cts;
+
+    private void Awake() {
+        subtitleText.gameObject.SetActive(false);
+    }
+
+    public void DisplaySubtitle(string subtitle)
+    {
+        _cts?.Cancel();
+        _cts = new CancellationTokenSource();
+        subtitleText.text = string.Empty;
+        subtitleText.gameObject.SetActive(true);
+        TypeAndHide(subtitle, _cts.Token).Forget();
+    }
+
+    private async UniTaskVoid TypeAndHide(string subtitle, CancellationToken token)
+    {
+        subtitleText.text = string.Empty;
+        for (int i = 0; i < subtitle.Length; i++)
+        {
+            token.ThrowIfCancellationRequested();
+            subtitleText.text += subtitle[i];
+            if (subtitle[i] == ',')
+            {
+                await UniTask.Delay((int)(_pauseOnCommaDuration * 1000), cancellationToken: token);
+            }
+            else
+            {
+                await UniTask.Delay((int)(_perCharacterTypeDuration * 1000), cancellationToken: token);
+            }
+        }
+        await UniTask.Delay((int)(_dissapearAfter * 1000), cancellationToken: token);
+        subtitleText.text = string.Empty;
+        subtitleText.gameObject.SetActive(false);
+    }
+}
