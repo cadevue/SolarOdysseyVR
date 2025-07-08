d793e70599f2c3d1b4a2d3cca0ee5ec70f4f97d3
Chris Albert <christiangultom5@gmail.com>
2025-07-08 17:09:04 +0700

    fix: solve scale issue on stencil

--- Assets/Scripts/UI/CopyCameraMove.cs
+++ Assets/Scripts/UI/CopyCameraMove.cs
@@ -10,12 +10,22 @@ public class CopyCameraMove : MonoBehaviour
     {
         if (playerCamera == null || anchorA == null || anchorB == null) return;
 
-        Vector3 localPos = anchorA.InverseTransformPoint(playerCamera.position);
-        Vector3 newWorldPos = anchorB.TransformPoint(localPos);
-        transform.position = newWorldPos;
+        float distA = Vector3.Distance(anchorA.position, playerCamera.position);
+        float distB = Vector3.Distance(anchorB.position, transform.position);
 
-        Quaternion localRot = Quaternion.Inverse(anchorA.rotation) * playerCamera.rotation;
-        Quaternion newWorldRot = anchorB.rotation * localRot;
-        transform.rotation = newWorldRot;
+        float scaleFactor = distA / distB;
+
+        anchorA.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+        var m = anchorB.localToWorldMatrix * anchorA.worldToLocalMatrix * playerCamera.localToWorldMatrix;
+        transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+
+        //Vector3 localPos = anchorA.InverseTransformPoint(playerCamera.position);
+        //Vector3 newWorldPos = anchorB.TransformPoint(localPos);
+        //transform.position = newWorldPos;
+
+        //Quaternion localRot = Quaternion.Inverse(anchorA.rotation) * playerCamera.rotation;
+        //Quaternion newWorldRot = anchorB.rotation * localRot;
+        //transform.rotation = newWorldRot;
     }
 }
