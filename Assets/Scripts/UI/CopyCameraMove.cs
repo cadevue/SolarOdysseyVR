ca1ed2168fc5cf2386f175ed630f414d52993bcf
Chris Albert <christiangultom5@gmail.com>
2025-07-09 16:02:22 +0700

    fix: inverted camera move

--- Assets/Scripts/UI/CopyCameraMove.cs
+++ Assets/Scripts/UI/CopyCameraMove.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public class CopyCameraMove : MonoBehaviour
 {
-    public Transform playerCamera;
-    public Transform anchorA;
-    public Transform anchorB;
+    [SerializeField] Transform playerCamera;
+    [SerializeField] Transform anchorA;
+    [SerializeField] Transform anchorB;
+
+    private void Awake()
+    {
+        float distA = Vector3.Distance(anchorA.position, playerCamera.position);
+        float distB = Vector3.Distance(anchorB.position, transform.position);
+
+        float scaleFactor = distA / distB;
+        anchorA.localScale = Vector3.one * scaleFactor;
+    }
 
     void LateUpdate()
     {
         if (playerCamera == null || anchorA == null || anchorB == null) return;
 
-        float distA = Vector3.Distance(anchorA.position, playerCamera.position);
-        float distB = Vector3.Distance(anchorB.position, transform.position);
-
-        float scaleFactor = distA / distB;
-
-        anchorA.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
-
         var m = anchorB.localToWorldMatrix * anchorA.worldToLocalMatrix * playerCamera.localToWorldMatrix;
-        transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+        transform.SetPositionAndRotation(m.GetColumn(3), ExtractRotation(m));
+    }
 
-        //Vector3 localPos = anchorA.InverseTransformPoint(playerCamera.position);
-        //Vector3 newWorldPos = anchorB.TransformPoint(localPos);
-        //transform.position = newWorldPos;
-
-        //Quaternion localRot = Quaternion.Inverse(anchorA.rotation) * playerCamera.rotation;
-        //Quaternion newWorldRot = anchorB.rotation * localRot;
-        //transform.rotation = newWorldRot;
+    private Quaternion ExtractRotation(Matrix4x4 m)
+    {
+        return Quaternion.LookRotation(m.GetColumn(2), m.GetColumn(1));
     }
 }
